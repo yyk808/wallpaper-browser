6aecad8b2d5fb506650f9348cb70ad188fb0741f
Neon <neonkk@qq.com>
2026-08-07 12:14:15 +0800

    Initial Commit

---
+++ wallpaper-browser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wallpaper-browser
+//
+//  Created by Neon on 2026/8/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wallpaper-browser/wallpaper_browserApp.swift
@@ -0,0 +1,17 @@
+//
+//  wallpaper_browserApp.swift
+//  wallpaper-browser
+//
+//  Created by Neon on 2026/8/7.
+//
+
+import SwiftUI
+
+@main
+struct wallpaper_browserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
